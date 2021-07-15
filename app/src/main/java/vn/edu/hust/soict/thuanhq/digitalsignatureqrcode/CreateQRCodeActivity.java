52e900ffcb6e9508db4f2bb5bb0122824fc7db8b
quangthuan952 <hoangquangthuan952@gmail.com>
2021-07-15 09:57:52 +0700

    fixed save QRCode to Gallery

--- app/src/main/java/vn/edu/hust/soict/thuanhq/digitalsignatureqrcode/CreateQRCodeActivity.java
+++ app/src/main/java/vn/edu/hust/soict/thuanhq/digitalsignatureqrcode/CreateQRCodeActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.ContentValues;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -24,11 +28,13 @@ import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.Hashtable;
 
 public class CreateQRCodeActivity extends AppCompatActivity {
-    private Button button;
+    private Button btnCreate;
     private Button btnSave;
     private EditText editText;
     private ImageView imageView;
@@ -36,11 +42,11 @@ public class CreateQRCodeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_q_r_code);
-        button = findViewById(R.id.button);
+        btnCreate = findViewById(R.id.button);
         btnSave = findViewById(R.id.saveQR);
         editText = findViewById(R.id.txt_QR);
         imageView = findViewById(R.id.iv_ouput);
-        button.setOnClickListener(new View.OnClickListener() {
+        btnCreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String sText = editText.getText().toString().trim();
@@ -72,7 +78,6 @@ public class CreateQRCodeActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 saveToGallery();
-
             }
         });
     }
@@ -80,28 +85,61 @@ public class CreateQRCodeActivity extends AppCompatActivity {
     public void saveToGallery() {
         BitmapDrawable bitmapDrawable = (BitmapDrawable) imageView.getDrawable();
         Bitmap bitmap = bitmapDrawable.getBitmap();
-        FileOutputStream outputStream = null;
-        File file = Environment.getExternalStorageDirectory();
-        File dir = new File(file.getAbsolutePath() + "/MyPics");
-        dir.mkdir();
-        String filename = String.format("%d.png", System.currentTimeMillis());
-        File outFile = new File(dir, filename);
-        try {
-            outputStream = new FileOutputStream(outFile);
-        } catch (Exception e) {
-            e.printStackTrace();
+
+        if (android.os.Build.VERSION.SDK_INT >= 29) {
+            ContentValues values = contentValues();
+            values.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/" + getString(R.string.app_name));
+            values.put(MediaStore.Images.Media.IS_PENDING, true);
+
+            Uri uri = this.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            if (uri != null) {
+                try {
+                    saveImageToStream(bitmap, this.getContentResolver().openOutputStream(uri));
+                    values.put(MediaStore.Images.Media.IS_PENDING, false);
+                    this.getContentResolver().update(uri, values, null, null);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        } else {
+            File directory = new File(Environment.getExternalStorageDirectory().toString() + '/' + getString(R.string.app_name));
+
+            if (!directory.exists()) {
+                directory.mkdirs();
+            }
+            String fileName = System.currentTimeMillis() + ".png";
+            File file = new File(directory, fileName);
+            try {
+                saveImageToStream(bitmap, new FileOutputStream(file));
+                ContentValues values = new ContentValues();
+                values.put(MediaStore.Images.Media.DATA, file.getAbsolutePath());
+                this.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
         }
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
-        try {
-            outputStream.flush();
-        } catch (Exception e) {
-            e.printStackTrace();
+        Toast.makeText(this, "Đã tải ảnh xuống!", Toast.LENGTH_SHORT).show();
+    }
+    private ContentValues contentValues() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/png");
+        values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis() / 1000);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
         }
-        try {
-            outputStream.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+        return values;
+    }
+
+    private void saveImageToStream(Bitmap bitmap, OutputStream outputStream) {
+        if (outputStream != null) {
+            try {
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+                outputStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-        Toast.makeText(this, "Đang tải ảnh xuống. Vui lòng đợi!", Toast.LENGTH_SHORT).show();
     }
 }
